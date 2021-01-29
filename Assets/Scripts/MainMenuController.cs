085b77787a180be5bc3ac11adfb42bf354273363
Emanuel Kotzayan <emanuelkotzayan@gmail.com>
2021-01-29 23:31:18 +0100

    Adding MainMenu, SceneHandler and AudioController

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Text MuteButtonText;
+
+    private SceneManagerController SceneManagerController;
+    private AudioController AudioController;
+
+    void Awake()
+    {
+        AudioController = GetComponent<AudioController>();
+        SceneManagerController = Object.FindObjectOfType<SceneManagerController>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)) 
+        {
+            OnClickStart();
+        }
+
+        MuteButtonText.text = AudioController.IsMuted ? "Unmute audio" : "Mute audio";
+    }
+
+    public void OnClickStart() 
+    {
+        SceneManagerController.GoToNexScene();
+    }
+
+    public void OnClickSound() 
+    {
+        AudioController.ToggleMute();
+    }
+}
