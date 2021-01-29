7a363392a6df2d550a753f4c95fb36ce2c627b8f
Emanuel Kotzayan <emanuelkotzayan@gmail.com>
2021-01-29 23:09:29 +0100

    Adding zoom out when still

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    const float MinOrthographicSize = 5f;
+    const float MaxOrthographicSize = 6f;
     const float Velocity = 10f;
     const float MovementThreshold = 0.1f;
-    private float MovementTimer = 0f;
+    const float StillThreshold = 1f;
 
+    private float MovementTimer = 0f;
+    private float StillTimer = 0f;
+
+    public CameraController Camera;
     private int dx, dy;
     private bool isMoving = false;
 
@@ -24,18 +30,32 @@ public class PlayerController : MonoBehaviour
         {
             dx = Input.GetAxis("Horizontal") > 0.01f ? 1 : (Input.GetAxis("Horizontal") < -0.01f ? -1 : 0);
             dy = dx != 0 ? 0 : (Input.GetAxis("Vertical") > 0.01f ? 1 : (Input.GetAxis("Vertical") < -0.01f ? -1 : 0));
-            isMoving = true;
+            isMoving = (dx != 0 || dy != 0);
         }
         if (isMoving)
         {
             float dt = Time.deltaTime;
             gameObject.transform.position += new Vector3(dx, dy, 0f) * dt * Velocity;
             MovementTimer += dt;
+            StillTimer = 0f;
             if (MovementTimer > MovementThreshold)
             {
                 MovementTimer = 0f;
                 isMoving = false;
             }
+        } 
+        else
+        {
+            StillTimer += Time.deltaTime;
+        }
+        
+        if (StillTimer > StillThreshold)
+        {
+            Camera.SetOrthographicSize(MaxOrthographicSize);
+        }
+        else 
+        {
+            Camera.SetOrthographicSize(MinOrthographicSize);
         }
     }
 
