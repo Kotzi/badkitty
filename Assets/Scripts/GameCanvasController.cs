de1f29bb6be9a40399c3e368f0c4be0f0bd53a53
Emanuel Kotzayan <emanuelkotzayan@gmail.com>
2021-01-30 18:18:49 +0100

    Fixes

--- Assets/Scripts/GameCanvasController.cs
+++ Assets/Scripts/GameCanvasController.cs
@@ -6,14 +6,15 @@ using UnityEngine.UI;
 public class GameCanvasController : MonoBehaviour
 {
      [SerializeField] Text countdownText;
-     [SerializeField] Text lisItems;
+     [SerializeField] Text FaceMaskText;
+     [SerializeField] Text KeysText;
+     [SerializeField] Text CarKeysText;
+     [SerializeField] Text WalletText;
 
      public void setCountdownText(string s){
             countdownText.text= s;
      }
      public void setListItems(string list){
-            lisItems.text = list;
-     }
-     
 
+     }
 }
