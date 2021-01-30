3699673a1b6ead6bb7065bda9578e42a0e7f1f27
wazyen <francisco.moreno02@estudiant.upf.edu>
2021-01-30 20:54:26 +0100

    More playable game

--- Assets/Scripts/InteractableObjects/Items/ItemContainer.cs
+++ Assets/Scripts/InteractableObjects/Items/ItemContainer.cs
@@ -9,8 +9,8 @@ public class ItemContainer : MonoBehaviour
 
     public GameObject player;
     public PlayerController player_controller;
-    public float distance_to_grab = 3f;
-    public float distance_to_shake = 4f;
+    public float distance_to_grab = 2.15f;
+    public float distance_to_shake = 3f;
     private ShakeCameraController Camera;
 
     // Start is called before the first frame update
@@ -25,7 +25,11 @@ public class ItemContainer : MonoBehaviour
     void Update()
     {
         float distance_to_player = (gameObject.transform.position - player.transform.position).magnitude;
-        //Debug.Log("distance_to_player: " + distance_to_player);
+        if (gameObject.name.Equals("ItemContainer4"))
+        {
+            Debug.Log("ItemContainer4 distance_to_player: " + distance_to_player);
+            Debug.Log("ItemContainer4 distance_to_grab: " + distance_to_grab);
+        }
         if (item != null && distance_to_player < distance_to_shake)
         {
             Camera.Shake(0.5f);
