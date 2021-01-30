320770d8ff97169591c4e3a576213783e03e7f6c
Emanuel Kotzayan <emanuelkotzayan@gmail.com>
2021-01-30 12:48:02 +0100

    Adding shake when near to an item

--- Assets/Scripts/Items/ItemContainer.cs
+++ Assets/Scripts/Items/ItemContainer.cs
@@ -10,12 +10,15 @@ public class ItemContainer : MonoBehaviour
     public GameObject player;
     public PlayerController player_controller;
     public float distance_to_grab = 3f;
+    public float distance_to_shake = 4f;
+    private ShakeCameraController Camera;
 
     // Start is called before the first frame update
     void Start()
     {
         player_controller = Object.FindObjectOfType<PlayerController>();
         player = player_controller.gameObject;
+        Camera = Object.FindObjectOfType<ShakeCameraController>();
     }
 
     // Update is called once per frame
@@ -23,6 +26,11 @@ public class ItemContainer : MonoBehaviour
     {
         float distance_to_player = (gameObject.transform.position - player.transform.position).magnitude;
         //Debug.Log("distance_to_player: " + distance_to_player);
+        if (item != null && distance_to_player < distance_to_shake)
+        {
+            Camera.Shake(0.5f);
+        }
+        
         if (Input.GetKeyDown(KeyCode.E) && distance_to_player < distance_to_grab)
         {
             if (item != null)
